05e35d657b6f2765bc4d62e42995ce3aa675f8a7
fullstack290623 <fullstack290623@gmail.com>
2024-03-20 21:14:57 +0200

    .

--- dals/company_dal.js
+++ dals/company_dal.js
@@ -1,115 +1,127 @@
-
 const knex = require('knex')
 const config = require('config')
 const logger = require('../logger/my_logger')
 
+const data_base = knex({
+    client: 'pg',
+    connection: {
+        host: config.db_connection.host,
+        user: config.db_connection.user,
+        password: config.db_connection.password,
+        database: config.db_connection.database
+    }
+})
+
 async function get_all_employees() {
+    // add try catch
     const employees = await data_base.raw("select * from company")
     employees.rows = employees.rows.map(e => {
         e.address = e.address.trimEnd();
         return e;
     })
     logger.debug(`app.get /api/employees number of records: ${employees.rows.length}`)
-    return employees.rows
+    return {
+        status: "success",
+        data: employees.rows
+    }
 }
 
-app.get('/api/employees/:id', async (request, response) => {
-    // add try catch
-    // if /:id is zero or not a number then return 400 with the error
-    // also log.error the error
-    // hint: typeof value === 'number' && !isNaN(value);
-    const id = request.params.id
+async function get_employee_by_id(id) {
     const employees = await data_base.raw(`select * from company where id = ${id}`)
     employees.rows = employees.rows.map(e => {
         e.address = e.address.trimEnd();
         return e;
     })
-    response.status(200).json(employees.rows)
-})
+    return {
+        status: "success",
+        data: employees.rows[0]
+    }    
+}
 
-app.post('/api/employees', async (request, response) => {
+async function insert_employee(new_employee) {
     try {
-        const new_employee = request.body
-        // await data_base.raw(`INSERT INTO company (name,age,address,salary) VALUES (?, ?, ?, ?);`,
-        //     [new_employee.name, new_employee.age, new_employee.address, new_employee.age])
         delete new_employee.id
         console.log(new_employee);
         const result_ids = await data_base('company').insert(new_employee).returning('id');
         console.log(result_ids[0]);
 
-        const id = result_ids[0].id
-        response.status(201).json({ new: { id, ...new_employee}, url: `/api/employees/${id}` })
-        // ...new_employee ==>   name: "Paul!", age: 32, address: "California!", salary: 20000
-        // {...new_employee} ==>  { name: "Paul!", age: 32, address: "California!", salary: 20000 }
-        // {id: result_ids[0].id, ...new_employee} ==>  
-        //            { id: 14, name: "Paul!", age: 32, address: "California!", salary: 20000 }
-        // /api/employees/8
+        const id = result_ids[0].id // the new id
+        return { status: "success", 
+                 data: { id, ...new_employee }  }
+        // url: `/api/employees/${id}`
     }
     catch (e) {
         logger.error(`Error in api post /api/employee. error = ${e.message}.`)
-        response.status(400).json({ status: "Failed to insert new employee", error: e.message.replaceAll("\"", "'") })
+        return {
+            status: "error",
+            internal: false,
+            error: e.message.replaceAll("\"", "'")
+        }
     }
-})
+}
 
-app.put('/api/employees/:id', async (request, response) => {
-    const id = request.params.id
-    const updated_employee = request.body
+async function update_employee(id, updated_employee) {
+    // add try-catch
+    // can fail due to duplication
     const result = await data_base.raw(`UPDATE company set name=?,age=?,address=?,salary=? where id=?`,
         [updated_employee.name, updated_employee.age, updated_employee.address ? updated_employee.address : '', updated_employee.salary, id])
-    response.status(200).json({ result: result.rowCount ? "employee updated" : "employee not found" })
-})
+    return {
+        status: "success",
+        data: result.rowCount
+    }    
+}
 
-app.patch('/api/employees/:id', async (request, response) => {
-    const id = request.params.id
-    // const { name ,address, salary, age } = request.body
-    const query = []
-    for (key in request.body) {
-        query.push(`${key}='${request.body[key]}'`)
+async function patch_employee(id, updated_employee) {
+    // add try-catch to figure out if there was
+    // a unique constrain error, if so the user should get error-400
+    const query_arr = []
+    for (let key in updated_employee) {
+        query_arr.push(`${key}='${updated_employee[key]}'`)
     }
 
-    // easy way
-    //const updated_employee = request.body
-    //const employee_db = await data_base.raw(`select * from company where id = ${id}`)
-    //const result = await data_base.raw(`UPDATE company set name=?,age=?,address=?,salary=? where id=?`,
-    //[updated_employee.name ? updated_employee.name : employee_db.name, 
-    //updated_employee.age, updated_employee.address ? updated_employee.address : '', updated_employee.salary, id])    
-
-    // name? query.push(`name='${name}'`) : null
-    // address? query.push(`address='${address}'`) : null
-    // salary? query.push(`salary=${salary}`) : null
-    // age? query.push(`age=${age}`) : null
-
-    if (query.length > 0) {
-        const query = `UPDATE company set ${query.join(', ')} where id=${id}`
+    if (query_arr.length > 0) {
+            // check how many employess updated?
+        const query = `UPDATE company set ${query_arr.join(', ')} where id=${id}`
         logger.debug(`app.patch employee ${query}`)
-        await data_base.raw(query)
-        response.status(200).json({ result: "employee patched" })
-        return
+        const result = await data_base.raw(query)
+        return {
+            status: "success",
+            data: result.rowCount
+        }         
     }
-    response.status(200).json({ result: "empty employee" })
-})
+    return {
+        status: "success",
+        data: query_arr.length
+    }          
+}
 
-app.delete('/api/employees/:id', async (request, response) => {
-    const id = request.params.id
+async function delete_employee(id) {
     const result = await data_base.raw(`DELETE from company where id=${id}`)
     console.log(result);
-    response.status(200).json({ result: result.rowCount ? "employee deleted" : "employee not found" })
-})
+    return {
+        status: "success",
+        data: result.rowCount
+    }  
+}
 
-app.delete('/api/employees-delete-table', async (request, response) => {
+async function delete_table() {
     // delete table
+    // try catch in case table does not exist
     await data_base.raw(`DROP table company`)
-    response.status(200).json({ status: "table-deleted" })
-})
+    return {
+        status: "success"
+    }      
+}
 
-app.post('/api/employees-create-table', async (request, response) => {
+async function create_table() {
     try {
-        logger.debug(request)
         await data_base.raw(`CREATE TABLE company (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE,` +
             `age INT NOT NULL,` +
             `address CHAR(50),` +
             `salary REAL);`);
-        response.status(201).json({ status: "table-created" })
+        return {
+            status: "success"
+        }          
     }
     catch (e) {
         const date = new Date();
@@ -126,20 +138,27 @@ app.post('/api/employees-create-table', async (request, response) => {
         console.log(`${formattedDate}.${date.getMilliseconds().toString().padStart(3, '0')} Error in employees-create-table. error = ${e} `);
         if (e.message.includes('already exists')) {
             logger.error(`Error in employees-create-table. error = ${e.message}.`)
-            response.status(400).json({ status: "Failed to create table", error: e.message.replaceAll("\"", "'") })
+            //response.status(400).json({ status: "Failed to create table", error: e.message.replaceAll("\"", "'") })
+            return {
+                status: "error",
+                internal: false,
+                error: e.message.replaceAll("\"", "'")
+            }              
         }
         else {
             const case_number = Math.floor(Math.random() * 1000000) + 1000000
             logger.error(`Case number (${case_number}): Error in employees-create-table. ${e.message}.`)
-            response.status(500).json({
-                status: "Failed to create table", error: `Internal error. please contact support ${case_number}`,
-                time: Date()
-            })
+            return {
+                status: "error",
+                internal: true,
+                error: `Case number (${case_number}): Error in employees-create-table. ${e.message}`
+            }              
         }
     }
-})
+}
 
-app.post('/api/employees-create6', async (request, response) => {
+async function insert_employees6() {
+    // add try-catch 
     `INSERT INTO company (name,age,address,salary)
     VALUES ('Paul', 32, 'California', 20000.00);
     INSERT INTO company (name,age,address,salary)
@@ -156,24 +175,13 @@ app.post('/api/employees-create6', async (request, response) => {
         .split(';')
         .filter(query => query)
         .forEach(async query => { await data_base.raw(query + ';') })
-    response.status(201).json({ result: "6 new employees created" })
-})
-
-// start
-app.listen(config.server.port, () => {
-    logger.info(`==== express server is up on port ${config.server.port}`);
-})
-
-const data_base = knex({
-    client: 'pg',
-    connection: {
-        host: config.db_connection.host,
-        user: config.db_connection.user,
-        password: config.db_connection.password,
-        database: config.db_connection.database
+    return {
+        status: "success"
     }
-})
+}
 
 module.exports = {
-    get_all_employees    
+    get_all_employees, get_employee_by_id, insert_employee,
+    update_employee, patch_employee, delete_employee, delete_table,
+    create_table, insert_employees6
 }
